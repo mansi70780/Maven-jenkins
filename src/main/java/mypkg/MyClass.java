8a6f90d758d32d739d7b0aa5ae94d8e7fadee2a0
mansi <abc@example.com>
2023-02-07 01:15:56 +0530

    first commit

---
+++ src/main/java/mypkg/MyClass.java
@@ -0,0 +1,15 @@
+package mypkg;
+
+public class MyClass {
+	public void show() {
+		System.out.println("Hello DevOps! welcome to jenkins");
+	}
+
+	public static void main(String[] args) {
+		MyClass ob=new MyClass();
+		ob.show();
+		
+
+	}
+
+}
